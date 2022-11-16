cc15ef045f35afbbf71b71a72b7399b2de9540a2
Cosme1 <cosme.schuenemsnn@gmail.com>
2022-11-16 20:41:49 +0100

    mostly done

---
+++ Kalender.dart
@@ -0,0 +1,39 @@
+import 'dart:io';
+
+int getdaysinMonth(int month) {
+  if (month == 2) {
+    return 28;
+  } else if (month == 4 || month == 6 || month == 9 || month == 11) {
+    return 30;
+  } else {
+    return 31;
+  }
+}
+
+void printCalendar(int month, int days) {
+  stdout.write("|Mo|Di|Mi|Do|Fr|Sa|So|");
+  var daysList = [];
+  var daysinMonth = getdaysinMonth(month);
+  for (int i = 1; i <= days; i++) {
+    daysList.insert(0, ' ');
+  }
+
+  for (int i = 1; i <= daysinMonth; i++) {
+    daysList.add(i.toString());
+  }
+  for (var i = 0; i < daysList.length; i++) {
+      if (i % 7 == 0) {
+        stdout.write('\n|');
+    }
+    if (daysList[i].length == 2) {
+      stdout.write(daysList[i] + '|');
+    }
+    else stdout.write(daysList[i] + ' |');
+    
+  }
+  
+}
+
+void main() {
+  printCalendar(2, 2);
+}
